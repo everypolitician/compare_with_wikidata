dddcf968e6e300d1aafd3afb3e1c0d5fb3bb2bf2
Mark Longair <mhl@pobox.com>
2017-07-21 13:31:42 +0100

    Extract the wikitext relating to single instances of our template

--- test/wikipage_test.rb
+++ test/wikipage_test.rb
@@ -7,23 +7,79 @@ class FakeClient
 
   def log_in(*); end
 
-  def get_wikitext(title)
-    FakeResponse.new('Foo bar')
+  SAMPLE_WIKITEXT = 'Hi, here is some introductory text.
+
+Now let\'s have a recognized template:
+
+{{Politician scraper comparison
+|foo=43
+|bar=Woolly Mountain Tapir
+}}
+
+<!-- COMPARISON OUTPUT BEGIN -->
+
+<!-- COMPARISON OUTPUT END -->
+
+And some other text here before a new template:
+
+{{Politician scraper comparison
+|quux=13
+|bar=Horse
+}}
+
+<!-- COMPARISON OUTPUT BEGIN -->
+
+<!-- COMPARISON OUTPUT END -->
+
+Now some trailing text.'.freeze
+
+  def get_wikitext(_title)
+    FakeResponse.new(SAMPLE_WIKITEXT)
   end
 end
 
 describe 'WikiPage' do
-  it 'can be initialized with a username, password and title' do
+  let(:wiki_page) do
     CompareWithWikidata::WikiPage.new(
-      username: 'john', password: 's3krit', title: 'Some Wiki page'
+      username: 'john', password: 's3krit', title: 'Some Wiki page', client_class: FakeClient
     )
   end
 
   it 'can return the wikitext for a page' do
+    wiki_page.wikitext.must_match(/^Hi, here is some.*trailing text.$/m)
+  end
 
-    wiki_page = CompareWithWikidata::WikiPage.new(
-      username: 'john', password: 's3krit', title: 'Some Wiki page', client_class: FakeClient
+  it 'returns two template sections' do
+    wiki_page.template_sections.length.must_equal(2)
+  end
+
+  it 'returns template sections of the expected class' do
+    wiki_page.template_sections[0].class.must_equal(CompareWithWikidata::TemplateSection)
+  end
+
+  it 'gives the right content to the first TemplateSection' do
+    wiki_page.template_sections[0].original_wikitext.must_equal(
+      '{{Politician scraper comparison
+|foo=43
+|bar=Woolly Mountain Tapir
+}}
+
+<!-- COMPARISON OUTPUT BEGIN -->
+
+<!-- COMPARISON OUTPUT END -->'
     )
-    wiki_page.wikitext.must_equal('Foo bar')
+  end
+
+  it 'gives the right content to the second TemplateSection' do
+    wiki_page.template_sections[1].original_wikitext.must_equal(
+      '{{Politician scraper comparison
+|quux=13
+|bar=Horse
+}}
+
+<!-- COMPARISON OUTPUT BEGIN -->
+
+<!-- COMPARISON OUTPUT END -->'
+    )
   end
 end
