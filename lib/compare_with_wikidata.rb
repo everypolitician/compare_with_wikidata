d0ae6806ca9d66a747bcb62fd1af868f42760611
Mark Longair <mhl@pobox.com>
2017-07-21 12:31:48 +0100

    Add a basic WikiPage class

--- lib/compare_with_wikidata.rb
+++ lib/compare_with_wikidata.rb
@@ -3,6 +3,7 @@ require 'compare_with_wikidata/version'
 require 'compare_with_wikidata/membership_list/morph'
 require 'compare_with_wikidata/membership_list/wikidata'
 require 'compare_with_wikidata/mapping/ep_identifier_to_wikidata'
+require 'compare_with_wikidata/wiki_api'
 
 module CompareWithWikidata
   # Your code goes here...
