0b853062bcc43d9321bdc78a7e05a91da1c24265
Chris Mytton <chrism@mysociety.org>
2017-07-26 08:27:25 +0100

    Add an example of using daff from Ruby
    This shows how to use daff from Ruby to compare two CSV-like arrays of
    values.
    
    This code was ported from the JavaScript example in the README [1]
    
    [1]: https://github.com/paulfitz/daff#readme

---
+++ experiments/daff.rb
@@ -0,0 +1,30 @@
+require 'daff'
+
+data1 = [
+  ['Country','Capital'],
+  ['Ireland','Dublin'],
+  ['France','Paris'],
+  ['Spain','Barcelona']
+]
+
+data2 = [
+  ['Country','Code','Capital'],
+  ['Ireland','ie','Dublin'],
+  ['France','fr','Paris'],
+  ['Spain','es','Madrid'],
+  ['Germany','de','Berlin']
+]
+
+t1 = Daff::TableView.new(data1)
+t2 = Daff::TableView.new(data2)
+
+alignment = Daff::Coopy.compare_tables(t1, t2).align
+
+data_diff = []
+table_diff = Daff::TableView.new(data_diff)
+
+flags = Daff::CompareFlags.new
+highlighter = Daff::TableDiff.new(alignment, flags)
+highlighter.hilite(table_diff)
+
+p data_diff
