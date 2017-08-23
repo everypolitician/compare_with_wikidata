3f3e1188f85a25fcb13b71acd547165b83dfb1f1
Chris Mytton <chrism@mysociety.org>
2017-08-23 12:24:07 +0100

    Rearrange Comparison class tests
    Move the `let` calls into the describe block so that we can provide
    different `sparql_items` and `csv_items` for each example.
    
    I've also changed the names of the describe blocks to be more about the
    example's context, rather than just the method name.

--- test/compare_with_wikidata/comparison_test.rb
+++ test/compare_with_wikidata/comparison_test.rb
@@ -1,25 +1,22 @@
 require 'test_helper'
 
 describe CompareWithWikidata::Comparison do
-  let(:sparql_items) { [{ id: 1, name: 'Alice' }, { id: 2, name: 'Bob' }] }
-  let(:csv_items) { [{ id: 1, name: 'Alice' }, { id: 2, name: 'Bob' }, { id: 3, name: 'Charlie' }] }
   subject { CompareWithWikidata::Comparison.new(sparql_items: sparql_items, csv_items: csv_items, columns: %i[id name]) }
 
-  describe '#headers' do
+  describe 'simple comparison' do
+    let(:sparql_items) { [{ id: 1, name: 'Alice' }, { id: 2, name: 'Bob' }] }
+    let(:csv_items) { [{ id: 1, name: 'Alice' }, { id: 2, name: 'Bob' }, { id: 3, name: 'Charlie' }] }
 
-    it 'returns the headers for the daff comparison' do
+    it 'has the expected headers' do
       subject.headers.must_equal ['@@', :id, :name]
     end
-  end
 
-  describe '#diff_rows' do
-    it 'returns the correct number of rows' do
+    it 'returns the correct number of diff_rows' do
       subject.diff_rows.size.must_equal 1
     end
 
-    it 'returns the expected number of DiffRow instances' do
-      row = subject.diff_rows.first
-      row.class.must_equal CompareWithWikidata::DiffRow
+    it 'returns DiffRow instances from diff_rows' do
+      subject.diff_rows.first.class.must_equal CompareWithWikidata::DiffRow
     end
   end
 end
